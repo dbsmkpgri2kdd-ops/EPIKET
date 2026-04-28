f76c3008c0e131a09a50b8b3a2127a33333347d8
dbsmkpgri2kdd-ops <dbsmkpgri2kdd@gmail.com>
2026-04-28 20:58:18 +0700

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'bioscan-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/@vladmandic/face-api/dist/face-api.js'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
